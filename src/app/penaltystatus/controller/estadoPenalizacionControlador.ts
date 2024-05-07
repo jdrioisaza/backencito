69eb94fd5f4a522a1567c0e89f3846b145084c6a
hacs199 <hernanchaverra.s@gmail.com>
2024-05-06 23:12:07 -0500

    estado penalizacion

--- src/app/penaltystatus/controller/estadoPenalizacionControlador.ts
+++ src/app/penaltystatus/controller/estadoPenalizacionControlador.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import EstadoPenalizacionDAO from "../dao/estadopenalizacionDAO";
+import EstadoPenalizacion from "../entity/estadoPenalizacion";
 
 class   EstadoPenalizacionControlador extends EstadoPenalizacionDAO {
 
@@ -7,6 +8,13 @@ class   EstadoPenalizacionControlador extends EstadoPenalizacionDAO {
 
         EstadoPenalizacionDAO.obtenerTodos([], res);
 
+    }
+
+    public cogeTuEstadoPenalizacion (req: Request, res: Response): void{
+        const objEstPen: EstadoPenalizacion = new EstadoPenalizacion(0, 0, 0);
+        objEstPen.idTipoEstadoPenalizacion = req.body.idTipoEstadoPenalizacion;
+        objEstPen.idPenalizacion = req.body.idPenalizacion;
+        EstadoPenalizacionDAO.grabeloYa(objEstPen, res);
     }
 }
 
--- src/app/penaltystatus/repository/estadopenalizacion_sql.ts
+++ src/app/penaltystatus/repository/estadopenalizacion_sql.ts
@@ -1,6 +1,12 @@
 export const SQL_ESTADO_PENALIZACION = {
     
-GET_ALL: "SELECT espe.id_estado_penalizacion, espe.id_tipo_estado_penalizacion, espe.id_penalizacion_estado_penalizacion \
-FROM estados_penalizaciones AS espe \
-ORDER BY espe.id_estado_penalizacion DESC",
+    GET_ALL: "SELECT espe.id_estado_penalizacion, espe.id_tipo_estado_penalizacion, espe.id_penalizacion_estado_penalizacion \
+    FROM estados_penalizaciones AS espe \
+    ORDER BY espe.id_estado_penalizacion DESC",
+
+    ADD: "INSERT INTO estados_penalizaciones(id_tipo_estado_penalizacion, id_penalizacion_estado_penalizacion) \
+    VALUES ($1, $2) RETURNING id_estado_penalizacion",
+
+    HOW_MANY: "SELECT COUNT(id_estado_penalizacion) as existe FROM estados_penalizaciones \
+    WHERE id_tipo_estado_penalizacion = $1",
 }
